880b8cc62f0b08bc640a8fbbc378e4a2ab436ae3
Prashan Raj Kc <64183754+prkprime1@users.noreply.github.com>
2020-09-13 21:06:02 +0545

    Login

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -2,16 +2,56 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using WebApplication12.ViewModal;
 
 namespace WebApplication12.Controllers
 {
     public class AccountController : Controller
     {
+        public readonly SignInManager<IdentityUser> SignInManager;
+        public readonly UserManager<IdentityUser> UserManager;
+
+        public AccountController(UserManager<IdentityUser> _UserManager, SignInManager<IdentityUser> _SignInManager)
+        {
+            this.UserManager = _UserManager;
+            this.SignInManager = _SignInManager;
+        }
+
+
         [HttpGet]
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModal model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser { UserName = model.Email, Email = model.Email };
+                var result = await UserManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    await SignInManager.SignInAsync(user, isPersistent: false);
+                    return RedirectToAction("index", "home");
+                } 
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description); 
+                }
+            }
+            return View();
+        }
+
+
+        public async Task<IActionResult> Logout()
+        {
+             await SignInManager.SignOutAsync();
+            return RedirectToAction("index", "home");
+
         }
     }
 }
