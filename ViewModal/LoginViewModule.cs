81cc1c4e69384c17484e97914f17b4c5c8caed2c
Prashan Raj Kc <64183754+prkprime1@users.noreply.github.com>
2020-09-15 11:55:15 +0545

    Login

---
+++ ViewModal/LoginViewModule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication12.ViewModal
+{
+    public class LoginViewModule
+    {
+        [Required]
+        [EmailAddress]
+        public int Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me")]
+        public bool RememberMe { get; set; }
+    }
+}
